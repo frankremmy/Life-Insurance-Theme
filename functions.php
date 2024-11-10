9d588e714b7ddf55527a0b97fa2dc7540c1d309d
frankremmy <princeugo.remix@gmail.com>
2024-11-10 01:25:50 +0100

    Create asset folders and enqueue them

--- functions.php
+++ functions.php
@@ -139,9 +139,11 @@ add_action( 'widgets_init', 'life_insurance_theme_widgets_init' );
  */
 function life_insurance_theme_scripts() {
 	wp_enqueue_style( 'life-insurance-theme-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_enqueue_style( 'life-insurance-main-style', get_template_directory_uri() . '/assets/css/style.css');
 	wp_style_add_data( 'life-insurance-theme-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'life-insurance-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'life-insurance-main-script', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), null, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
